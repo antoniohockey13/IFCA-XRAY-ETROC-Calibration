700e0f36b9745a4d7ff598f5b06d7f96cffa140d
Antonio <antoniogomezcarrera@gmail.com>
2024-12-17 10:26:44 +0100

    File to plot hit map, tot and toa

---
+++ plot_hit_map_tot_toa.py
@@ -0,0 +1,114 @@
+import sifca_utils.plotting
+import ROOT
+import click
+import sifca_utils
+
+sifca_utils.plotting.set_sifca_style()
+
+# Set ROOT to batch mode if plots are omitted
+omit_plots = False
+ROOT.gROOT.SetBatch(omit_plots)
+
+# Map sensor positions
+SENSOR_POS = {"6": 1, "7": 3, "8": 4, "9": 2}
+
+def hit_map(df):
+    """
+    Plot the hit map of the ETROC. It is a matrix 16x16
+    Args:
+        df (ROOT.RDataFrame): RDataFrame with the hits
+    """
+    c = ROOT.TCanvas()
+    c.SetRightMargin(0.2) 
+    hit_map = df.Histo2D(("hit_map", "Hit map", 16, 0., 16., 16, 0., 16.), "col", "row")
+    hit_map.GetXaxis().SetTitle("Column")
+    hit_map.GetYaxis().SetTitle("Row")
+    hit_map.GetZaxis().SetTitle("Hits")
+    hit_map.Draw("colz")
+    c.Update()
+    c.Draw()
+    input("Press enter to continue...")
+
+def ToT(df):
+    """
+    Plot the ToT of the ETROC in ns
+    
+    Args:
+        df (ROOT.RDataFrame): RDataFrame with the hits
+    """
+    canvas = ROOT.TCanvas()
+    canvas.Divide(2, 2)
+
+    histograms = []
+
+    for col, pos in SENSOR_POS.items():
+        canvas.cd(pos)
+        hist = df.Filter(f"row==15 && col=={col}").Histo1D(
+            ("ToT", f"Column {col}", 150, 0., 7.), "ToT"
+        )
+        hist.GetXaxis().SetTitle("ToT/ns")
+        hist.GetYaxis().SetTitle(f"Counts col={col}")
+        hist.SetTitle(f"Column {col}")
+        hist.Draw()
+
+        histograms.append(hist)
+
+    canvas.Update()
+    if not omit_plots:
+        # Keep the canvas open until user input
+        input("Press Enter to continue...")  
+
+def ToA(df):
+    """
+    Plot the ToA of the ETROC in ns
+    
+    Args:
+        df (ROOT.RDataFrame): RDataFrame with the hits
+    """
+    canvas = ROOT.TCanvas()
+    canvas.Divide(2, 2)
+
+    histograms = []
+
+    for col, pos in SENSOR_POS.items():
+        canvas.cd(pos)
+        hist = df.Filter(f"row==15 && col=={col}").Histo1D(
+            ("ToA", f"Column {col}", 75, 0., 14.), "ToA"
+        )
+        hist.GetXaxis().SetTitle("ToA/ns")
+        hist.GetYaxis().SetTitle(f"Counts col={col}")
+        hist.SetTitle(f"Column {col}")
+        hist.Draw()
+
+        histograms.append(hist)
+
+    canvas.Update()
+    if not omit_plots:
+        # Keep the canvas open until user input
+        input("Press Enter to continue...")
+
+
+@click.command()
+@click.argument('inputfile', nargs=1)
+def main(inputfile):
+    # Open the file
+    if inputfile.split('.')[-1] != 'root':
+        raise ValueError(f"Input file must have .root extension and it has .{inputfile.split('.')[-1]}")
+    
+    f = ROOT.TFile.Open(inputfile)
+    df = ROOT.RDataFrame("Hits", f)
+
+    # Plot the hit map
+    hit_map(df)
+
+    # Plot the ToT
+    ToT(df)
+
+    # Plot the ToA
+    ToA(df)
+
+if __name__ == "__main__":
+    try:
+        main()
+    except ValueError as e:
+        print(f"\033[91mError: {e}\033[0m")
