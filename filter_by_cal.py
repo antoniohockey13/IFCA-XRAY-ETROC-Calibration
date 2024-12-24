53bb22d57044602ab5744cfd4b5c29f732580c7b
AntonioHockey13 <antoniogomezcarrera@gmail.com>
2024-12-24 11:00:16 +0100

    Store data with analogical/digital voltage supply

--- filter_by_cal.py
+++ filter_by_cal.py
@@ -11,7 +11,7 @@ omit_plots = False
 ROOT.gROOT.SetBatch(omit_plots)
 
 # Map sensor positions
-SENSOR_POS = {"6": 1, "7": 3, "8": 4, "9": 2}
+# SENSOR_POS = {"6": 1, "7": 3, "8": 4, "9": 2}
 
 
 def plot_cal_histograms(df, title = "Cal values before filtering"):
@@ -20,23 +20,35 @@ def plot_cal_histograms(df, title = "Cal values before filtering"):
     Cal goes from 0 to 1023 in integer values.
     """
     canvas = ROOT.TCanvas("c", title)
-    canvas.Divide(2, 2)
+    canvas.Divide(2, 1)
 
     max_cal_bins = {}
     histograms = []
-
-    for col, pos in SENSOR_POS.items():
-        canvas.cd(pos)
-        hist = df.Filter(f"row==15 && col=={col}").Histo1D(
-            ("cal", f"Column {col}", 1024, 0., 1023), "cal"
-        )
+    for i in range(2):
+        canvas.cd(i+1)
+        hist = df.Filter(f"Analogical_HV == {i}").Histo1D(
+            ("cal", f"Analogical_HV = {i}", 1024, 0., 1023), "cal"
+            )
         hist.GetXaxis().SetTitle("Cal")
-        hist.GetYaxis().SetTitle(f"Counts col={col}")
-        hist.SetTitle(f"Column {col}")
+        hist.GetYaxis().SetTitle(f"Counts Analogical HV={i}")
+        hist.SetTitle(f"Analogical HV = {i}")
         hist.Draw()
 
         histograms.append(hist)
-        max_cal_bins[col] = hist.GetMaximumBin()
+        max_cal_bins[i] = hist.GetMaximumBin()
+    
+    # for col, pos in SENSOR_POS.items():
+    #     canvas.cd(pos)
+    #     hist = df.Filter(f"row==15 && col=={col}").Histo1D(
+    #         ("cal", f"Column {col}", 1024, 0., 1023), "cal"
+    #     )
+    #     hist.GetXaxis().SetTitle("Cal")
+    #     hist.GetYaxis().SetTitle(f"Counts col={col}")
+    #     hist.SetTitle(f"Column {col}")
+    #     hist.Draw()
+
+    #     histograms.append(hist)
+    #     max_cal_bins[col] = hist.GetMaximumBin()
 
     canvas.Update()
     if not omit_plots:
@@ -58,17 +70,23 @@ def main(inputfiles):
         # Open file and create RDataFrame
         f = ROOT.TFile(file)
         df = ROOT.RDataFrame("Hits", f)
-
+        
+        # Define column to know if analogical or digital voltage supply
+        df = df.Define("Analogical_HV", "floor(col/8)")
         # Draw histogram with Cal values and get max cal bin for each sensor column
         max_cal = plot_cal_histograms(df=df, title="Cal values before filtering")
 
         # Filter and define new columns
         # Build filter expression
-        filter_expr = " || ".join(
-            [f"row==15 && col=={col} && abs(cal-{max_cal[col]})<2.5" for col in max_cal])
+        condition = []
+        for i in max_cal:
+            condition.append(f"Analogical_HV == {i} && abs(cal-{max_cal[i]})<2.5")
+
+        filter_expr = " || ".join(condition)
+
         # Apply filter
         df_filtered = df.Filter(filter_expr, "Cal cut")
-        print(df.Report().Print())
+        print(df_filtered.Report().Print())
 
         # Plot cal after filetering
         plot_cal_histograms(df_filtered, title="Cal values after filtering")
@@ -86,8 +104,9 @@ def main(inputfiles):
         # Save filtered data
         tree_name = "Hits"
         file_name = f"{folder}/Filtered_{name}"
-        columns = {"row", "col", "cal", "ToA", "ToT", "t_bin"}
+        columns = {"row", "col", "cal", "ToA", "ToT", "t_bin", "Analogical_HV"}
         df_filtered.Snapshot(tree_name, file_name, columns)
+        print(f"New ROOT file saved in {file_name}")
         
 if __name__ == '__main__':
     try:
