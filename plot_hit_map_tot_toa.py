447c4a98dafe7525ddac35ab5a37fbbda7275ef9
AntonioHockey13 <antoniogomezcarrera@gmail.com>
2025-01-14 16:37:08 +0100

    Added function to plot ToA per pixel and corrected bin size of ToA_CODE to 2*bin to be consistent with ToA

--- plot_hit_map_tot_toa.py
+++ plot_hit_map_tot_toa.py
@@ -124,8 +124,13 @@ def ToA_CODE(df):
 
     for i in range(2):
         canvas.cd(i+1)
+        bin_size = 2*1
+        min_toa = -bin_size/2
+        max_toa = 1024+bin_size/2
+        bin_number = int((max_toa-min_toa)/bin_size)
+        print(f"Number of bins: {bin_number}")
         hist = df.Filter(f"Analogical_HV == {i}").Histo1D(
-            ("toa_code", f"Analogical HV {i}", 1025, -0.5, 1024.5), "toa_code"
+            ("toa_code", f"Analogical HV {i}", bin_number, min_toa, max_toa), "toa_code"
         )
         hist.GetXaxis().SetTitle("ToA_CODE")
         hist.GetYaxis().SetTitle(f"Counts")
@@ -139,6 +144,52 @@ def ToA_CODE(df):
         # Keep the canvas open until user input
         input("Press Enter to continue...")
 
+def ToA_pixel(df):
+    """
+    Plot the ToA of the ETROC in for each column 6, 7, 8 and 9. 
+    This function is prepared for the wirebounded ETROCs.
+
+    Args:
+        df (ROOT.RDataFrame): RDataFrame with the hits
+    
+    Returns:
+        histograms (list): List with the histograms for each pixel
+    """
+    POSITIONS = {"6": 1, "7": 3, "8": 4, "9": 2}
+    canvas = ROOT.TCanvas()
+    canvas.Divide(2, 2)
+    histograms = []
+    for i in range(6, 10):
+        canvas.cd(POSITIONS[str(i)])
+
+        # Filter dataframe to select col and obtain t_bin
+        df_i = df.Filter(f"col == {i}")
+        t_bin = df_i.Mean("t_bin").GetValue()
+        print(f"Row {i} t_bin: {t_bin}")
+        bin_size = 2*t_bin
+        min_toa = -bin_size/2
+        max_toa = 14+bin_size/2
+        bin_number = int((max_toa-min_toa)/bin_size)    
+
+        # Create histogram
+        hist = df_i.Histo1D(
+            ("ToA", f"Row {i}", bin_number, min_toa, max_toa), "ToA"
+        )
+        # Configure histogram
+        hist.GetXaxis().SetTitle("ToA/ns")
+        hist.GetYaxis().SetTitle(f"Counts")
+        hist.SetTitle(f"Column = {i}")
+        hist.Draw()
+
+        histograms.append(hist)
+
+    canvas.Update()
+    if not omit_plots:
+        # Keep the canvas open until user input
+        input("Press Enter to continue...")
+    return histograms
+
+    
 
 def t_bin(df):
     """
@@ -232,16 +283,17 @@ def main(inputfile):
     df = ROOT.RDataFrame("Hits", f)
 
     # Plot the hit map
-    hit_map(df)
+    # hit_map(df)
 
-    # Plot the ToT
-    ToT(df)
+    # # Plot the ToT
+    # ToT(df)
 
     # Plot the ToT together
     # ToT_together(df)
     # Plot the ToA
     ToA(df)
     ToA_CODE(df)
+    ToA_pixel(df)
 
     # Plot the t_bin
     # t_bin(df)
