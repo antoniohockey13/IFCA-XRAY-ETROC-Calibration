4f447aa49902f8f50bccd3852b1fc8ed5eeee9a4
AntonioHockey13 <antoniogomezcarrera@gmail.com>
2025-01-20 13:32:31 +0100

    Added per pixel functions, prepared for only 4 pixels in columns 6 7 8 9

--- plot_ToAs.py
+++ plot_ToAs.py
@@ -4,6 +4,22 @@ import sifca_utils
 
 sifca_utils.plotting.set_sifca_style()
 colors = [ROOT.kRed, ROOT.kBlue, ROOT.kGreen]
+SENSOR_POS = {"6": 1, "7": 3, "8": 4, "9": 2}
+
+
+def get_histograms_limits(t_bin):
+    """
+    Get the limits for the histograms
+    
+    Args:
+        t_bin (float)
+    """
+    bin_size = 2*t_bin
+    min_toa = -bin_size/2
+    max_toa = 14+bin_size/2
+    bin_number = int((max_toa-min_toa)/bin_size)
+    return min_toa, max_toa, bin_number
+
 
 def draw_toa_together(df_dict):
     """
@@ -43,20 +59,68 @@ def draw_toa_together(df_dict):
             df_i_filtered = df_i.Filter(f"Analogical_HV == {ipad}")
             # Get t_bin for bin size and compute limits
             t_bin = df_i_filtered.Mean("t_bin").GetValue()
-            print(f"Analogical HV {ipad}, bin selected: {i}, t_bin: {t_bin}")
-            bin_size = 2*t_bin 
-            min_tot = -bin_size/2
-            max_tot = 14+bin_size/2
-            bin_number = int((max_tot-min_tot)/bin_size)
-
+            min_toa, max_toa, bin_number = get_histograms_limits(t_bin)
             # Create histogram
             histograms[ipad].append(df_i_filtered.Histo1D(
-                ("ToA", f"Analogical HV {ipad}", bin_number, min_tot, max_tot), "ToA"
+                ("ToA", f"Analogical HV {ipad}", bin_number, min_toa, max_toa), "ToA"
             ))
             histograms[ipad][-1].SetDirectory(0)
             histograms[ipad][-1].SetLineColor(colors[int(i)+1])
             legends[-1].AddEntry(histograms[ipad][-1].GetValue(), f"Bin selected {i}", "l")
             histograms[ipad][-1].Draw(opt)
+        legends[-1].Draw()
+    c.Update()
+    input("Press enter to continue...")
+
+def draw_toa_together_per_pixel(df_dict):
+    """
+    Draw the ToA histograms in the same canvas normalised for the different bins selected
+
+    Args:
+        df_dict (dict): Dictionary with the different dataframes
+    """
+    # Y-axis limits of the histogras
+    limits = {}
+    limits[6] = 300
+    limits[7] = 3e3
+    limits[8] = 1.2e4
+    limits[9] = 400
+    # Create canvas
+    c = ROOT.TCanvas()
+    # Divide it for columns
+    c.Divide(2,2)
+    histograms = {6: [], 7: [], 8: [], 9: []}
+    legends = []
+    # Loop over the columns
+    for icol in range(6, 10):
+        c.cd(SENSOR_POS[str(icol)])
+        # Remove statistics values from the histogram
+        ROOT.gStyle.SetOptStat(000000)
+        # Create limits histogram
+        histograms[icol].append(ROOT.TH2F("limits", "", 1, 0, 14, 1, 1e-3, limits[icol]))
+        histograms[icol][-1].Draw()
+        histograms[icol][-1].GetXaxis().SetTitle("ToA/ns")
+        histograms[icol][-1].GetYaxis().SetTitle("Counts")
+        histograms[icol][-1].SetTitle(f"Column = {icol}")
+        c.Draw()
+        opt = "same"
+        legends.append(ROOT.TLegend(0.2, 0.8, 0.5, 0.9))
+
+        # Loop over the selected bins
+        for i, df_i in df_dict.items():
+            # Filter selected dataframe with the column
+            df_i_filtered = df_i.Filter(f"col == {icol}")
+            # Get t_bin for bin size and compute limits
+            t_bin = df_i_filtered.Mean("t_bin").GetValue()
+            min_toa, max_toa, bin_number = get_histograms_limits(t_bin)
+            # Create histogram
+            histograms[icol].append(df_i_filtered.Histo1D(
+                ("ToA", f"Column {icol}", bin_number, min_toa, max_toa), "ToA"
+            ))
+            histograms[icol][-1].SetDirectory(0)
+            histograms[icol][-1].SetLineColor(colors[int(i)+1])
+            legends[-1].AddEntry(histograms[icol][-1].GetValue(), f"Bin selected {i}", "l")
+            histograms[icol][-1].Draw(opt)
         legends[-1].Draw()
     c.Update()
     input("Press enter to continue...")
@@ -85,18 +149,14 @@ def draw_toa_stacked(df_dict, df_15):
         df_15_filtered = df_15.Filter(f"Analogical_HV == {ipad}")
         # Get t_bin for bin size and compute limits
         t_bin = df_15_filtered.Mean("t_bin").GetValue()
-        print(f"Analogical HV {ipad}, bins selected -1, 0 and 1, t_bin: {t_bin}")        
-        bin_size = 2*t_bin
-        min_tot = -bin_size/2
-        max_tot = 14+bin_size/2
-        bin_number = int((max_tot-min_tot)/bin_size)
+        min_toa, max_toa, bin_number = get_histograms_limits(t_bin)
         # Loop over the selected bins
         for i, df_i in df_dict.items():
             # Filter selected dataframe with te Analogical HV
             df_i_filtered = df_i.Filter(f"Analogical_HV == {ipad}")
             print(f"Analogical HV {ipad}, bin selected: {i}")
             histograms[ipad].append(df_i_filtered.Histo1D(
-                ("ToA", f"Analogical HV {ipad}", bin_number, min_tot, max_tot), "ToA"
+                ("ToA", f"Analogical HV {ipad}", bin_number, min_toa, max_toa), "ToA"
             ).GetValue()
             )
 
@@ -120,6 +180,68 @@ def draw_toa_stacked(df_dict, df_15):
     c.Update()
     input("Press enter to continue...")
     
+def draw_toa_stacked_per_pixel(df_dict, df_15):
+    """
+    Draw the ToA histograms in the same canvas stacked for the different bins selected
+
+    Args:
+        df_dict (dict): Dictionary with the different dataframes
+        df_15 (ROOT.RDataFrame): Dataframe with the data for the bin selected 1.5
+    """
+    
+    # Create Canvas
+    c = ROOT.TCanvas()
+    # Divide it for different pixels
+    c.Divide(2,2)
+    histograms = {6: [], 7: [], 8: [], 9: []}
+    legends = []
+    stack = {
+        6: ROOT.THStack("stack6", f"Column 6"), 
+        7: ROOT.THStack("stack7", f"Column 7"),
+        8: ROOT.THStack("stack8", f"Column 8"),
+        9: ROOT.THStack("stack9", f"Column 9")
+        }
+    
+    for icol in range(6,10):
+        c.cd(SENSOR_POS[str(icol)])
+        legends.append(ROOT.TLegend(0.8, 0.8, 0.9, 0.9))
+        # Filter df_15
+        df_15_filtered = df_15.Filter(f"col == {icol}")
+        # Get t_biAnalogical HV = {ipad}n for bin size and compute limits
+        t_bin = df_15_filtered.Mean("t_bin").GetValue()
+        min_toa, max_toa, bin_number = get_histograms_limits(t_bin)
+
+        # Loop over the selected bins
+        for i, df_i in df_dict.items():
+            # Filter selected dataframe with te Analogical HV
+            df_i_filtered = df_i.Filter(f"col == {icol}")
+
+            histograms[icol].append(df_i_filtered.Histo1D(
+                ("ToA", f"Column {icol}", bin_number, min_toa, max_toa), "ToA"
+            ).GetValue()
+            )
+
+            histograms[icol][-1].SetDirectory(0)
+            legends[-1].AddEntry(histograms[icol][-1], f"Bin selected {i}", "l")
+            histograms[icol][-1].SetLineColor(colors[int(i)+1])
+            # Set fill characteristic
+            histograms[icol][-1].SetFillColorAlpha(colors[int(i)+1], 1)
+            histograms[icol][-1].SetFillStyle(3001)
+
+        # Sort histograms by the number of entries first the one with more entries
+        histograms[icol].sort(key=lambda x: x.GetEntries(), reverse=True)
+        for i, histo_i in enumerate(histograms[icol]):
+            stack[icol].Add(histo_i)
+        
+        stack[icol].Draw("hist fill")
+        stack[icol].GetXaxis().SetTitle("ToA/ns")
+        stack[icol].GetYaxis().SetTitle("Counts")
+        stack[icol].SetTitle(f"Column =  {icol}")
+        legends[-1].Draw()
+    c.Update()
+    input("Press enter to continue...")
+
+
 @click.command()
 @click.argument('inputfiles', nargs=-1)
 def main(inputfiles):
@@ -146,9 +268,11 @@ def main(inputfiles):
             f = ROOT.TFile.Open(inputfile)
             df_15 = ROOT.RDataFrame("Hits", f)
     
-    # draw_toa_together(df_dict)
+    draw_toa_together(df_dict)
+    draw_toa_together_per_pixel(df_dict)
 
     draw_toa_stacked(df_dict, df_15)
+    draw_toa_stacked_per_pixel(df_dict, df_15)
 
 if __name__ == "__main__":
     main()
