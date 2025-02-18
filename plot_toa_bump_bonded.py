691693de33f8e465ee38076be677232d03d7391e
AntonioHockey13 <antoniogomezcarrera@gmail.com>
2025-02-18 19:03:07 +0100

    Improved df filtering

--- plot_toa_bump_bonded.py
+++ plot_toa_bump_bonded.py
@@ -45,8 +45,8 @@ def draw_toa_stacked(df_dict):
     for df_i in df_dict.values():
         t_bin.append(df_i.Mean("t_bin").GetValue())
     min_toa, max_toa, bin_number = get_histograms_limits(np.mean(np.array(t_bin)))
+    
     for i_color, (i, df_i) in enumerate(df_dict.items()):
-
         # Create histogram
         histograms.append(df_i.Histo1D(
             ("ToA", f"", bin_number, min_toa, max_toa), "ToA"
@@ -86,7 +86,7 @@ def draw_toa_normalised(df_dict):
     # filter 0 : 7e-3
     # filter 1 : 0.05
     # filter -1 : 0.04
-    histograms.append(ROOT.TH2F("limits", "", 1, 0, 14, 1, 1e-3, 7e-3))
+    histograms.append(ROOT.TH2F("limits", "", 1, 0, 14, 1, 1e-3, 0.018))
     histograms[-1].Draw()
     histograms[-1].GetXaxis().SetTitle("ToA/ns")
     histograms[-1].GetYaxis().SetTitle("Counts")
@@ -95,11 +95,14 @@ def draw_toa_normalised(df_dict):
     opt = "same"
     legend = ROOT.TLegend(0.2, 0.8, 0.5, 0.9)
 
+    # Loop over df to select binning as the mean
+    t_bin = []
+    for df_i in df_dict.values():
+        t_bin.append(df_i.Mean("t_bin").GetValue())
+    min_toa, max_toa, bin_number = get_histograms_limits(np.mean(np.array(t_bin)))
+
     # Loop over dfs to plot them
     for i_color, (i, df_i) in enumerate(df_dict.items()):
-        # Compute binning
-        t_bin = df_i.Mean("t_bin").GetValue()
-        min_toa, max_toa, bin_number = get_histograms_limits(t_bin)
         # Create histogram
         histograms.append(df_i.Histo1D(
                 ("ToA", f"", bin_number, min_toa, max_toa), "ToA"
@@ -109,7 +112,7 @@ def draw_toa_normalised(df_dict):
         histograms[-1].SetLineColor(colors[i_color])
         histograms[-1].SetMarkerColor(colors[i_color])
         legend.AddEntry(histograms[-1], f"{i}", "l")
-        histograms[-1].DrawNormalized(opt+"P")
+        histograms[-1].DrawNormalized(opt)
         legend.Draw()
     c.Draw()
     input("Press enter to continue...")
@@ -158,8 +161,9 @@ def main(inputfiles):
         inputfiles (list): List with the input files format expected: */*-Time.*
 
     """
-    Analogical_LV = 0
-    print(f"Filter to Analogical_LV = {Analogical_LV}")
+    df_filters = "col == 5 && row == 6"
+
+    print(f"Filter: {df_filters}")
     df_dict_run = {}
     for inputfile in inputfiles:
         name = inputfile.split("/")[-1].split("-")
@@ -170,12 +174,12 @@ def main(inputfiles):
         hour = name[-1].split(".")[0]
         name = f"{date}-{hour}__{filter}"
         f = ROOT.TFile.Open(inputfile)
-        df_dict_run[name] = ROOT.RDataFrame("Hits", f).Filter(f"Analogical_LV == {Analogical_LV}")
-    
+        df = ROOT.RDataFrame("Hits", f)
+        df_dict_run[name] = ROOT.RDataFrame("Hits", f).Filter(df_filters)
+
     draw_toa_stacked(df_dict_run)
     draw_toa_normalised(df_dict_run)
     if len(df_dict_run) == 2:
         draw_toa_substraction(df_dict_run)
 if __name__ == "__main__":
     main()
-    
