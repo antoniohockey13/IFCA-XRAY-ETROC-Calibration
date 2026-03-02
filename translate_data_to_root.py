7ae68556e449fa4377364b1a179cff170e98e7ee
angomezc <antonio.gomez.carrera@cern.ch>
2026-03-02 16:09:33 +0100

    New translate function

--- translate_data_to_root.py
+++ translate_data_to_root.py
@@ -4,6 +4,8 @@ import os
 from array import array
 from tqdm import tqdm
 
+MAX_8BIT = 2**8
+
 def create_array():
     """
     Create an array of integers with one element.
@@ -33,6 +35,7 @@ def main(inputfiles):
     os.makedirs(out_dir, exist_ok=True)
     out_path = os.path.join(out_dir, f"{run_folder}.root")
     hits = ROOT.TFile(out_path, "RECREATE")
+    hits_tree = ROOT.TTree("Hits", "Hits")
 
     # Format of lines:
     # EH version event_number hits_count num_words
@@ -51,7 +54,7 @@ def main(inputfiles):
         'channel_h': create_array(),
         'l1counter_hw': create_array(), 
         'l1counter_global': create_array(), 
-        'type': create_array(),
+        'type_': create_array(),
         'bcid': create_array(),
         # D
         'channel_d': create_array(),
@@ -71,179 +74,168 @@ def main(inputfiles):
         'et_overflow_count': create_array(),
         'et_hamming_count': create_array(),
         'et_crc': create_array(),
-        # GLobal event counter
-        'event_id': create_array(),
+        # Gobal event counter
+        'event_id_': create_array(),
     }
 
 
-    hits_tree = ROOT.TTree("Hits", "Hits")
     for var_name, var in variables.items():
         hits_tree.Branch(var_name, var, f"{var_name}/I")
 
-
-    event_id = 1
-    previous_hw_event = None
-    overflow_counter = 0
-    event_at_reset = 0 
-    previous_l1 = None
-    l1_overflow = 0
-    number_of_resets = 0
-    MAX_8BIT = 256
-
-    MAX_16BIT = 65535
-
-    import re
-
-    def extract_number(filename):
-        # Extrae el último número que aparezca en el nombre
-        numbers = re.findall(r'\d+', os.path.basename(filename))
-        return int(numbers[-1]) if numbers else -1
-
-    sorted_files = sorted(inputfiles, key=extract_number)
-
-    for inputfile in tqdm(sorted_files):
-        with open(inputfile) as f:
-            lines = f.readlines()
+    with open(inputfiles[0]) as f:
+        lines = f.readlines()
             
-        iline = 0
-        nlines = len(lines)
+    iline = 0
+    nlines = len(lines)
 
-        while iline < nlines:
-            # Init of block must be EH
-            if not lines[iline].startswith('EH'):
-                iline += 1
-                continue
-            # Next line must be H
-            if iline + 1 >= nlines or not lines[iline + 1].startswith('H'):
-                iline += 1
-                continue
+    previous_l1_hw = -1 
+    previous_event_number = -1
+    global_event_counter = 0
+    global_l1_counter = 0
+    events_at_reset = 0
+    events_at_l1_reset = 0
+    reset_number = 0
 
-                
-            l1_hw = int(lines[iline + 1].split()[2])
+    while iline < nlines-3:
+        if lines[iline].startswith("EH") and lines[iline+1].startswith("H"):
+            eh_line = lines[iline]
+            h_line = lines[iline + 1]
+        else:
+            iline += 1 
+            continue
 
-            if previous_l1 is not None:
-                if previous_l1 > 200 and l1_hw < 50:
-                    l1_overflow += 1
-
-            l1_global = l1_overflow * MAX_8BIT + l1_hw
-            previous_l1 = l1_hw
-
-            variables['l1counter_hw'][0] = l1_hw
-            variables['l1counter_global'][0] = l1_global
-
-            # Parse EH y H
-            try:
-                parts = lines[iline].split()
-                hw_event = int(parts[1]) # 16-bit
-                event_type = int(parts[2])
-                num_words = int(parts[3])
-                if previous_hw_event is not None:
-                    if  hw_event <= previous_hw_event:
-                        overflow_counter += 1
-                        event_at_reset = previous_hw_event + event_at_reset
-                        if hw_event == 0:
-                            event_at_reset += 1
-                        print(f"Reset detected at line {iline} in file {inputfile}, previous_hw_event: {previous_hw_event}, current_hw_event: {hw_event}, total overflows: {overflow_counter}")
-            
-                global_event_number = event_at_reset + hw_event
-
-
-                variables['event_number'][0] = global_event_number
-                variables['event_type'][0] = event_type
-                variables['num_words'][0] = num_words
-
-                previous_hw_event = hw_event
-                if previous_hw_event == 0:
-                    number_of_resets += 1
-                    print(f"Reset detected at line {iline}, total resets: {number_of_resets}")
-                    print(f"Global event numer at reset: {global_event_number}")
-
-                variables['channel_h'][0], variables['l1counter_hw'][0], variables['type'][0], variables['bcid'][0] = \
-                    map(int, lines[iline + 1].split()[1:])
-
-                if inputfile == "/media/elenamr/B6EB-8B99/2Kintex/K2/run_569/file_242.sem":
-                    print(f"{global_event_number}")
-                    print(f"Event ID = {event_id}")
-
-
-                #if event_id != global_event_number:
-                #    print(f"Warning: event_id {event_id} does not match global_event_number {global_event_number} at line {iline} in file {inputfile}")    
-                #    input()    
-            except Exception:
-                iline += 1
-                continue
-
-           # Read all consecutive D lines until T is found
-            j = iline + 2
-            d_lines = []
-            while j < nlines and lines[j].startswith('D'):
-                d_lines.append(lines[j])
-                j += 1
-
-            # T must be right after D lines
-            if j >= nlines or not lines[j].startswith('T'):
-                iline += 1
-                continue
-
-            # Parse T once
-            try:
-                variables['channel_t'][0], variables['status'][0], variables['hits_t'][0], variables['crc'][0] = \
-                    map(int, lines[j].split()[1:])
-            except Exception:
-                iline += 1
-                continue
-
-            variables['event_id'][0] = event_id
-            event_id += 1
-
-            # Parse ET
-            if j + 1 < nlines and lines[j + 1].startswith('ET'):
-                try:
-                    parts_et = lines[j + 1].split()
-                    # Ejemplo esperado: "ET num_hits overflow_count hamming_count crc"
-                    variables['et_num_hits'][0] = int(parts_et[1])
-                    variables['et_overflow_count'][0] = int(parts_et[2])
-                    variables['et_hamming_count'][0] = int(parts_et[3])
-                    variables['et_crc'][0] = int(parts_et[4])
-                except Exception:
-                    variables['et_num_hits'][0] = -1
-                    variables['et_overflow_count'][0] = -1
-                    variables['et_hamming_count'][0] = -1
-                    variables['et_crc'][0] = -1
+        data_line = True
+        i_data = 0
+        d_lines = []
+        while data_line:
+            if lines[iline+2+i_data].startswith("D"):
+                # print(lines[iline + 2 + i_data])
+                d_lines.append(lines[iline+2+i_data])
+                i_data+=1
+                # print(i_data)
             else:
-                variables['et_num_hits'][0] = -1
-                variables['et_overflow_count'][0] = -1
-                variables['et_hamming_count'][0] = -1
-                variables['et_crc'][0] = -1
+                data_line = False
 
-            # Fill once per hit
-            for d in d_lines:
-                try:
-                    (variables['channel_d'][0], variables['ea'][0], variables['row'][0],
-                     variables['col'][0], variables['toa_code'][0],
-                     variables['tot_code'][0], variables['cal'][0]) = \
-                        map(int, d.split()[1:])
+        # print(lines[iline + 2 + i_data])
+        # print(lines[iline + 3 + i_data])
+        if lines[iline+2+i_data].startswith("T") and lines[iline+3+i_data].startswith("ET"):
+            t_line = lines[iline + 2 + i_data]
+            et_line = lines[iline + 3 + i_data]
+        else:
+            print(f"ERROR, unexpected line in {iline}")
+            print(lines[iline])
+            print(lines[iline+1])
+            print(lines[iline+2])
+            print(lines[iline+1+i_data])
+            print(lines[iline+2+i_data])
+            input()
 
-                    hits_tree.Fill()
+        # Parse lines
+        # EH event_number, event_type, num_words
+        # print(eh_line.split())
+        _, event_number, event_type, num_words, _ = eh_line.split()
+        event_number = int(event_number)
+        event_type = int(event_type)
+        num_words = int(num_words)
+        # H channel, l1_counter, type, bcid
+        # print(h_line.split())
+        _, channel_h, l1_counter_hw, type_, bcid = h_line.split()
+        channel_h = int(channel_h)
+        l1_counter_hw = int(l1_counter_hw)
+        type_ = int(type_)
+        bcid = int(bcid)
+        # T channel, status, hits, crc
+        # print(t_line.split())
+        parts = t_line.split()
+        channel_t = int(parts[1])
+        status = int(parts[2])
+        hits_t = int(parts[3])
+        crc_t = int(parts[4])
+        # ET num_hits, overflow_count, hamming_count, crc
+        # print(et_line.split())
+        _, num_hits, overflow_count, hamming_count, crc_et = et_line.split()
+        num_hits = int(num_hits)
+        overflow_count = int(overflow_count)
+        hamming_count = int(hamming_count)
+        crc_et = int(crc_et)
 
-                except Exception:
-                    continue
+        # Compute global counters
+        if event_number <= previous_event_number:
+            print(f"Found reset in line {iline}, at previous event number = {previous_event_number} and event number = {event_number}, number of resets = {reset_number}")
+            reset_number += 1
+            events_at_reset += previous_event_number
+        if event_number == 0:
+            events_at_reset += 1
+        global_event_counter = events_at_reset + event_number
+        
+        if l1_counter_hw < previous_l1_hw:
+            # print(f"Found reset in line {iline}, at previous l1 = {previous_l1_hw} and l1 counter= {l1_counter_hw}")
+            # print(lines[iline])
+            events_at_l1_reset += previous_l1_hw
+        if l1_counter_hw == 0:
+            events_at_l1_reset += 1
+        global_l1_counter = events_at_l1_reset + l1_counter_hw
 
-            if j + 1 < nlines and lines[j + 1].startswith('ET'):
-                iline = j + 2
-            else:
-                iline = j + 1
 
-
+        for i_data_line in d_lines:
+            try:
+                # D channel, EA, Row, Col, ToA, ToT, Cal
+                # print(i_data_line.split())
+                _, channel_d, ea, row, col, toa, tot, cal = i_data_line.split()
+                channel_d = int(channel_d)
+                ea = int(ea)
+                row = int(row)
+                col = int(col)
+                toa = int(toa)
+                tot = int(tot)
+                cal = int(cal)
+                # Fill the tree
+                variables['event_number'][0]      = event_number
+                variables['event_type'][0]        = event_type
+                variables['num_words'][0]         = num_words
+                variables['channel_h'][0]         = channel_h
+                variables['l1counter_hw'][0]      = l1_counter_hw
+                variables['l1counter_global'][0]  = global_l1_counter
+                variables['type_'][0]             = type_
+                variables['bcid'][0]              = bcid
+                variables['channel_d'][0]         = channel_d
+                variables['ea'][0]                = ea
+                variables['col'][0]               = col
+                variables['row'][0]               = row
+                variables['toa_code'][0]          = toa
+                variables['tot_code'][0]          = tot
+                variables['cal'][0]               = cal
+                variables['channel_t'][0]         = channel_t
+                variables['status'][0]            = status
+                variables['hits_t'][0]            = hits_t
+                variables['crc'][0]               = crc_t
+                variables['et_num_hits'][0]       = num_hits
+                variables['et_overflow_count'][0] = overflow_count
+                variables['et_hamming_count'][0]  = hamming_count
+                variables['et_crc'][0]            = crc_et
+                variables['event_id_'][0]         = global_event_counter
+                # print(event_number, event_type, num_words, channel_h, l1_counter_hw, global_l1_counter, type_, bcid, channel_d, ea, col, row, toa, tot, cal, channel_t, status, hits, crc_t, num_hits, overflow_count, hamming_count, crc_et, global_event_counter)
+                hits_tree.Fill()
+            except (ValueError, IndexError):
+                print("ERROR")
+                continue
+        # if global_l1_counter != global_event_counter:
+        #     print("ERROR: different number of events")
+        #     print(f"global event = {global_event_counter}, l1 event = {global_l1_counter}")
+        #     print(f"line = {iline}")
+        #     # input()
+        # Update counters 
+        iline += 4 + i_data
+        previous_event_number = event_number
+        previous_l1_hw = l1_counter_hw
             
             
     hits_tree.Write()
     hits.Close()
 
     print(f"File saved in {out_path}")
-    print(f"Total reconstructed events: {event_id}")
-    print(f"Last global event number: {global_event_number}")
-    print(f"Last L1 counter value: {l1_global}")
+    print(f"Total reconstructed events: {global_event_counter}")
+    print(f"Last L1 counter value: {global_l1_counter}")
 
 if __name__ == '__main__':
     try:
